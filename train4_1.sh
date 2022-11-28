ba9f5a28b03d0b897791e94f97d200b2c76d3569
benlin1211 <ben1211.eecs06@nctu.edu.tw>
2022-11-28 15:51:22 +0800

    eval 4-1 change config path

--- train4_1.sh
+++ train4_1.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 python DirectVoxGO/run.py --config ./train4_1_config.py --no_reload --render_test
+
+# python DirectVoxGO/run.py --config ./train4_1_config.py --eval_ssim --eval_lpips_alex --eval_lpips_vgg --render_val
+
